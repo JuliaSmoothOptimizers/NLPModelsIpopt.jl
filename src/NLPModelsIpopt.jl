b3d926ec17dffa3c00259c5e07a606fa48b42e0f
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-02-07 10:09:53 -0500

    Computes time by looking at the output

--- src/NLPModelsIpopt.jl
+++ src/NLPModelsIpopt.jl
@@ -10,6 +10,7 @@ Solves the `NLPModel` problem `nlp` using `IpOpt`.
 """
 function ipopt(nlp :: AbstractNLPModel;
                callback :: Union{Function,Nothing} = nothing,
+               ignore_time :: Bool = false,
                kwargs...)
   n, m = nlp.meta.nvar, nlp.meta.ncon
   eval_f(x) = obj(nlp, x)
@@ -51,22 +52,48 @@ function ipopt(nlp :: AbstractNLPModel;
                           eval_jac_g, eval_h)
   problem.x = copy(nlp.meta.x0)
 
+  print_output = true
+
   # Options
   for (k,v) in kwargs
-    addOption(problem, string(k), v)
+    if ignore_time || k != :print_level || v ≥ 3
+      addOption(problem, string(k), v)
+    else
+      if v > 0
+        @warn("`print_level` should be 0 or ≥ 3 to get the elapsed time, if you don't care about the elapsed time, pass `ignore_time=true`")
+      end
+      print_output = false
+      addOption(problem, "print_level", 3)
+    end
   end
 
   # Callback
   callback === nothing || setIntermediateCallback(problem, callback)
 
-  status = solveProblem(problem)
+  tmpfile = tempname()
+  local status
+  open(tmpfile, "w") do io
+    redirect_stdout(io) do
+      status = solveProblem(problem)
+    end
+  end
+  ipopt_output = readlines(tmpfile)
+
+  cpu_time_lines = filter(line->occursin("CPU secs", line), ipopt_output)
+  Δt = 0.0
+  for line in cpu_time_lines
+    Δt += Meta.parse(split(line, "=")[2])
+  end
+  if print_output
+    println(join(ipopt_output, "\n"))
+  end
   x  = problem.x
   c  = problem.g
   λ  = problem.mult_g
   zL = problem.mult_x_L
   zU = problem.mult_x_U
   f  = problem.obj_val
-  return x, f, c, λ, zL, zU, Ipopt.ApplicationReturnStatus[status]
+  return x, f, c, λ, zL, zU, Ipopt.ApplicationReturnStatus[status], Δt
 end
 
 end # module
