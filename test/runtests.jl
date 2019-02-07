86f0c0f4ca532c21d32a2b9cb741219eacce2ff4
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-02-07 10:09:53 -0500

    Computes time by looking at the output

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,23 +2,23 @@ using NLPModelsIpopt, NLPModels, Ipopt, Test
 
 function tests()
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  x, f, c, λ, zL, zU, status = ipopt(nlp)
+  x, f, c, λ, zL, zU, status, Δt = ipopt(nlp)
   @test isapprox(x, [1.0; 1.0], rtol=1e-6)
 
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2),
                    c=x->[sum(x) - 1.0], lcon=[0.0], ucon=[0.0])
-  x, f, c, λ, zL, zU, status = ipopt(nlp)
+  x, f, c, λ, zL, zU, status, Δt = ipopt(nlp)
   @test isapprox(x, [-1.4; 2.4], rtol=1e-6)
 
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  x, f, c, λ, zL, zU, status = ipopt(nlp, tol=1e-12)
+  x, f, c, λ, zL, zU, status, Δt = ipopt(nlp, tol=1e-12)
   @test isapprox(x, [1.0; 1.0], rtol=1e-6)
 
   function callback(alg_mod, iter_count, args...)
     return iter_count < 1
   end
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  x, f, c, λ, zL, zU, status = ipopt(nlp, tol=1e-12, callback=callback)
+  x, f, c, λ, zL, zU, status, Δt = ipopt(nlp, tol=1e-12, callback=callback)
   @test status == :User_Requested_Stop
 end
 
