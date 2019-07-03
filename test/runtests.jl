ed1548d62bf8febdcb9e06dc26e79abee76b442f
Dominique Orban <dominique.orban@gmail.com>
2019-07-03 15:40:47 -0700

    return number of iterations
    closes #13

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,18 @@ function tests()
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
   stats = ipopt(nlp)
   @test isapprox(stats.solution, [1.0; 1.0], rtol=1e-6)
+  @test stats.iter == 21
 
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2),
                    c=x->[sum(x) - 1.0], lcon=[0.0], ucon=[0.0])
   stats = ipopt(nlp)
   @test isapprox(stats.solution, [-1.4; 2.4], rtol=1e-6)
+  @test stats.iter == 1
 
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
   stats = ipopt(nlp, tol=1e-12)
   @test isapprox(stats.solution, [1.0; 1.0], rtol=1e-6)
+  @test stats.iter == 22
 
   function callback(alg_mod, iter_count, args...)
     return iter_count < 1
@@ -20,6 +23,7 @@ function tests()
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
   stats = ipopt(nlp, tol=1e-12, callback=callback)
   @test stats.solver_specific[:internal_msg] == :User_Requested_Stop
+  @test stats.iter == 1
 end
 
 tests()
