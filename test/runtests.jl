c786b2454b54e5747375ae155ee8d25a3eda66db
Dominique Orban <dominique.orban@gmail.com>
2019-07-17 17:14:36 -0400

    test initial point

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,6 +17,12 @@ function tests()
   @test isapprox(stats.solution, [1.0; 1.0], rtol=1e-6)
   @test stats.iter == 22
 
+  # solve again from solution
+  x0 = copy(stats.solution)
+  stats = ipopt(nlp, x0=x0, tol=1e-12)
+  @test isapprox(stats.solution, x0, rtol=1e-6)
+  @test stats.iter == 0
+
   function callback(alg_mod, iter_count, args...)
     return iter_count < 1
   end
