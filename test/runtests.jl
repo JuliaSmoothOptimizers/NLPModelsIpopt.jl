a8cd614b90bd822f041ba68ca984f8af0f937956
dpo <dpo@users.noreply.github.com>
2021-05-26 13:15:23 +0000

    :robot: Format .jl files

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,19 @@ using ADNLPModels, NLPModelsIpopt, NLPModels, Ipopt, Test
 
 function tests()
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  stats = ipopt(nlp, print_level=0)
-  @test isapprox(stats.solution, [1.0; 1.0], rtol=1e-6)
+  stats = ipopt(nlp, print_level = 0)
+  @test isapprox(stats.solution, [1.0; 1.0], rtol = 1e-6)
   @test stats.status == :first_order
 
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  stats = ipopt(nlp, tol=1e-12, print_level=0)
-  @test isapprox(stats.solution, [1.0; 1.0], rtol=1e-6)
+  stats = ipopt(nlp, tol = 1e-12, print_level = 0)
+  @test isapprox(stats.solution, [1.0; 1.0], rtol = 1e-6)
   @test stats.status == :first_order
 
   # solve again from solution
   x0 = copy(stats.solution)
-  stats = ipopt(nlp, x0=x0, tol=1e-12, print_level=0)
-  @test isapprox(stats.solution, x0, rtol=1e-6)
+  stats = ipopt(nlp, x0 = x0, tol = 1e-12, print_level = 0)
+  @test isapprox(stats.solution, x0, rtol = 1e-6)
   @test stats.status == :first_order
   @test stats.iter == 0
 
@@ -22,15 +22,15 @@ function tests()
     return iter_count < 1
   end
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  stats = ipopt(nlp, tol=1e-12, callback=callback, print_level=0)
+  stats = ipopt(nlp, tol = 1e-12, callback = callback, print_level = 0)
   @test stats.status == :user
   @test stats.solver_specific[:internal_msg] == :User_Requested_Stop
   @test stats.iter == 1
 
-  nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2),
-                   x->[sum(x) - 1.0], [0.0], [0.0])
-  stats = ipopt(nlp, print_level=0)
-  @test isapprox(stats.solution, [-1.4; 2.4], rtol=1e-6)
+  nlp =
+    ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2), x -> [sum(x) - 1.0], [0.0], [0.0])
+  stats = ipopt(nlp, print_level = 0)
+  @test isapprox(stats.solution, [-1.4; 2.4], rtol = 1e-6)
   @test stats.iter == 1
   @test stats.status == :first_order
 
@@ -39,21 +39,21 @@ function tests()
   y0 = copy(stats.multipliers)
   zL = copy(stats.multipliers_L)
   zU = copy(stats.multipliers_U)
-  stats = ipopt(nlp, x0=x0, y0=y0, zL0=zL, zU0=zU, print_level=0)
-  @test isapprox(stats.solution, x0, rtol=1e-6)
-  @test isapprox(stats.multipliers, y0, rtol=1e-6)
-  @test isapprox(stats.multipliers_L, zL, rtol=1e-6)
-  @test isapprox(stats.multipliers_U, zU, rtol=1e-6)
+  stats = ipopt(nlp, x0 = x0, y0 = y0, zL0 = zL, zU0 = zU, print_level = 0)
+  @test isapprox(stats.solution, x0, rtol = 1e-6)
+  @test isapprox(stats.multipliers, y0, rtol = 1e-6)
+  @test isapprox(stats.multipliers_L, zL, rtol = 1e-6)
+  @test isapprox(stats.multipliers_U, zU, rtol = 1e-6)
   @test stats.iter == 0
   @test stats.status == :first_order
 
-  meta = NLPModelMeta(1, x0=rand(1), lvar=zeros(1), uvar=ones(1), minimize=false)
+  meta = NLPModelMeta(1, x0 = rand(1), lvar = zeros(1), uvar = ones(1), minimize = false)
   nlp = ADNLPModel(meta, Counters(), ADNLPModels.ForwardDiffAD(), x -> x[1], x -> [])
-  stats = ipopt(nlp, print_level=0)
-  @test isapprox(stats.solution, ones(1), rtol=1e-6)
-  @test isapprox(stats.objective, 1.0, rtol=1e-6)
-  @test isapprox(stats.multipliers_L, zeros(1), atol=1e-6)
-  @test isapprox(stats.multipliers_U, -ones(1), rtol=1e-6)
+  stats = ipopt(nlp, print_level = 0)
+  @test isapprox(stats.solution, ones(1), rtol = 1e-6)
+  @test isapprox(stats.objective, 1.0, rtol = 1e-6)
+  @test isapprox(stats.multipliers_L, zeros(1), atol = 1e-6)
+  @test isapprox(stats.multipliers_U, -ones(1), rtol = 1e-6)
   @test stats.status == :first_order
 end
 
