ac2a7ff16adb6c7a5ffa79f411e388bbd8e7564f
Tangi Migot <tangi.migot@gmail.com>
2025-02-18 07:19:26 -0500

    Add more tests and robust file check (#127)
    * Add more tests and robust file check
    
    * loose

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,25 +7,40 @@ using ADNLPModels, NLPModelsIpopt, NLPModels, Ipopt, SolverCore, Test
   stats = solve!(solver, nlp, stats, print_level = 0)
   @test isapprox(stats.solution, [1.0; 1.0], rtol = 1e-6)
   @test stats.status == :first_order
+  @test stats.iter == 21
+  @test stats.elapsed_time > 0
+  @test stats.primal_feas ≈ 0.0
+  @test stats.dual_feas ≈ 0.0 atol = 1.49e-8
+
   nlp = ADNLPModel(x -> (x[1])^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
   reset!(solver, nlp)
   stats = solve!(solver, nlp, stats, print_level = 0)
   @test isapprox(stats.solution, [0.0; 0.0], atol = 1e-6)
   @test stats.status == :first_order
+  @test stats.iter == 16
+  @test stats.elapsed_time > 0
+  @test stats.primal_feas ≈ 0.0
+  @test stats.dual_feas ≈ 0.0 atol = 1.49e-8
 end
 
-function tests()
+@testset "Unit tests NLPModelsIpopt" begin
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
   stats = ipopt(nlp, print_level = 0)
   @test isapprox(stats.solution, [1.0; 1.0], rtol = 1e-6)
   @test stats.status == :first_order
   @test stats.elapsed_time > 0
+  @test stats.iter == 21
+  @test stats.primal_feas ≈ 0.0
+  @test stats.dual_feas ≈ 0.0 atol = 1.49e-8
 
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
   stats = ipopt(nlp, tol = 1e-12, print_level = 0)
   @test isapprox(stats.solution, [1.0; 1.0], rtol = 1e-6)
   @test stats.status == :first_order
   @test stats.elapsed_time > 0
+  @test stats.iter == 22
+  @test stats.primal_feas ≈ 0.0
+  @test stats.dual_feas ≈ 0.0
 
   # solve again from solution
   x0 = copy(stats.solution)
@@ -33,6 +48,9 @@ function tests()
   @test isapprox(stats.solution, x0, rtol = 1e-6)
   @test stats.status == :first_order
   @test stats.iter == 0
+  @test stats.elapsed_time >= 0
+  @test stats.primal_feas ≈ 0.0
+  @test stats.dual_feas ≈ 0.0
 
   function callback(alg_mod, iter_count, args...)
     return iter_count < 1
@@ -43,6 +61,8 @@ function tests()
   @test first(stats.solver_specific[:internal_msg]) == :User_Requested_Stop
   @test stats.iter == 1
   @test stats.elapsed_time > 0
+  @test stats.primal_feas ≈ 0.0
+  # @test stats.dual_feas ≈ 4.63
 
   nlp =
     ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2), x -> [sum(x) - 1.0], [0.0], [0.0])
@@ -51,6 +71,8 @@ function tests()
   @test stats.iter == 1
   @test stats.status == :first_order
   @test stats.elapsed_time > 0
+  @test stats.primal_feas ≈ 0.0
+  @test stats.dual_feas ≈ 0.0 atol = 1.49e-8
 
   # solve constrained problem again from solution
   x0 = copy(stats.solution)
@@ -65,8 +87,11 @@ function tests()
     @test isapprox(stats.multipliers_L, zL, rtol = 1e-6)
     @test isapprox(stats.multipliers_U, zU, rtol = 1e-6)
   end
+  @test stats.elapsed_time >= 0
   @test stats.iter == 0
   @test stats.status == :first_order
+  @test stats.primal_feas ≈ 0.0
+  @test stats.dual_feas ≈ 0.0 atol = 1.49e-8
 
   x0, f = rand(1), x -> x[1]
   nlp = ADNLPModel(f, x0, zeros(1), ones(1), minimize = false)
@@ -78,6 +103,7 @@ function tests()
   @test isapprox(stats.multipliers_U, -ones(1), rtol = 1e-6)
   @test stats.status == :first_order
   @test stats.elapsed_time > 0
+  @test stats.iter == 5
+  @test stats.primal_feas ≈ 0.0
+  @test stats.dual_feas ≈ 0.0 atol = 1.49e-8
 end
-
-tests()
